058a23bb38635634e945cbe32b39ed8fea03eb22
Reshy <reshy@protonmail.com>
2022-01-10 17:33:31 +0100

    Add components

---
+++ app/javascript/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import Greeting from './Greeting'
+import store from '../store'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Main = () => {
+    return (
+        <React.StrictMode>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routes>
+                    <Route exact path="/" element={<Greeting/>}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </React.StrictMode>
+    )
+}
+
+export default Main
